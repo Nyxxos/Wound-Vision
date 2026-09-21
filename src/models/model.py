25cd20febb11f6719f36a0e12fc508954bda13d8
Haroon Hussein <hussein.haroon@outlook.com>
2026-09-21 20:05:44 +0100

    Updated model.py

--- src/models/model.py
+++ src/models/model.py
@@ -1,4 +1,13 @@
 import torch.nn as nn
 from torchvision.models import resnet18, ResNet18_Weights
 
-model = resnet18(weights = ResNet18_Weights.IMAGENET1K_V1)
+def wound_vision_model(num_classes):
+    model = resnet18(weights = ResNet18_Weights.IMAGENET1K_V1)
+    for parameters in model.parameters():
+        parameters.requires_grad = False
+    model.fc = nn.Linear(model.fc.in_features, num_classes)
+    return model
+
+
+
+
